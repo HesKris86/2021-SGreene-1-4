8363771bf13c999166ce1a843491e03ec950b570
HesKris86 <points2the.watch@gmail.com>
2021-03-05 09:32:52 -0500

    solo app

---
+++ my_app_2.py
@@ -0,0 +1,172 @@
+import time
+import random
+
+
+kiesha_reply = ['**spits out water! lol', 'You lyin. Aint nobody mama raise them that way!', 'You play too much. LOL']
+
+#print(random.choice(kiesha_reply))
+hey_there = "Hey There."
+print(hey_there.center(40, '-'))
+time.sleep(2)
+print("My name is Keisha.")
+time.sleep(2)
+print("I will remain with you throughout the survey.")
+time.sleep(2)
+
+
+user1= input("What is your name? ")
+
+time.sleep(2)
+being_mean = "That is sooo ugly!!!"
+print(being_mean.center(30,'*'))
+
+time.sleep(1)
+print('Just playing.')
+time.sleep(1)
+input(f'Thanks {user1}. I can not wait to get started.\nPlease press enter.')
+time.sleep(2)
+
+print('Today we are going to take a survey to learn some random facts about you.')
+time.sleep(1)
+print('This should be fun.')
+print("please use the letter to match your desired answer.")
+my_list = ['a: a lot', 'b: some', 'c: none']
+print(my_list)
+q_1 = input(' Question: 1 How much do you like ketchup? ')
+
+if q_1 == "a":
+    print("That is amazing! ")
+elif q_1 == "b":
+    print('You should be better!')
+else:
+    print("sorry to hear that.")
+print(random.choice(kiesha_reply))
+kiesha_reply = ['**spits out water! lol', 'Aint nobody mama raise them that way!', 'You play too much. LOL']
+for reply in kiesha_reply:
+    if reply == kiesha_reply[1]:
+        print('just playing.')
+time.sleep(2)
+random.choice(kiesha_reply)
+print(my_list)
+q_2 = input('Question: 2 Do you like the NBA? ')
+if q_2 == "a":
+    print("I bet your favorite team sucks ")
+elif q_2 == "b":
+    print('That is ok. The sport can be overrated!')
+else:
+    print("sorry to hear that.")
+print(random.choice(kiesha_reply))
+for reply in kiesha_reply:
+    if reply == kiesha_reply[2]:
+        print('dont be out here crying.')
+time.sleep(2)
+print(my_list)
+q_3 = input('Question: 3 Do you eat cheese? ')
+if q_3 == "a":
+    print("That is so wrong. lol! ")
+elif q_3 == "b":
+    print('Try a fried grilled cheese sandwich.')
+else:
+    print("Yor're weird.")
+print(random.choice(kiesha_reply))
+for reply in kiesha_reply:
+    if reply == kiesha_reply[0]:
+        print('Imma tell on you. lol')
+time.sleep(2)
+print(my_list)
+q_4 = input('Question: 4 How much salt do you use? ')
+if q_4 == "a":
+    print("Be careful ")
+elif q_4 == "b":
+    print('Dont let your food be too bland')
+else:
+    print("Never cook for me.")
+print(random.choice(kiesha_reply))
+for reply in kiesha_reply:
+    if reply == kiesha_reply[1]:
+        print('just playing.')
+time.sleep(2)
+print(my_list)
+
+q_5 = input('Question: 5 Do you like to go swimming? ')
+if q_5 == "a":
+    print("Thats a cool skillset. A lot of people can not. ")
+elif 5 == "b":
+    print('I hope you drown. Just kidding.')
+else:
+    print("Get outside and live a little.")
+print(random.choice(kiesha_reply))
+for reply in kiesha_reply:
+    if reply == kiesha_reply[2]:
+        print('dont be out here crying.')
+time.sleep(2)
+print(my_list)
+q_6 = input("Question: 6 How much of the Super Bowl did you watch? ")
+if q_6 == "a":
+    print('Go Tampa Bay ')
+elif q_6 == "b":
+    print('I do not blame you. The refs cheated. ')
+else:
+    print("You are a communist!")
+print(random.choice(kiesha_reply))
+for reply in kiesha_reply:
+    if reply == kiesha_reply[0]:
+        print('Imma tell on you. lol')
+time.sleep(2)
+print(my_list)
+q_7 = input('Question: 7 Do you smoke? ')
+if q_7 == "a":
+    print("**inserts eyes emojoi ")
+elif q_7 == "b":
+    print('You had better!')
+else:
+    print("You must be the FEDs")
+print(random.choice(kiesha_reply))
+time.sleep(2)
+print(my_list)
+q_8 = input('Question: 8 Do you enjoy the smell of outdoors? ')
+if q_8 == "a":
+    print("That is amazing! ")
+elif q_8 == "b":
+    print('Try to get outside more often.')
+else:
+    print("sorry to hear that.")
+print(random.choice(kiesha_reply))
+time.sleep(2)
+print(my_list)
+q_9 = input('Question: 9 How much money you got? ')
+if q_9 == "a":
+    print("Look out for the cookout ")
+elif q_9 == "b":
+    print('Throw me something. Cmon, you know I am good for it.')
+else:
+    print("Broke a$$")
+print(random.choice(kiesha_reply))
+for reply in kiesha_reply:
+    if reply == kiesha_reply[2]:
+        print('dont be out here crying.')
+time.sleep(2)
+print(my_list)
+q_10 = input('Question: 10 How much did you enjoy this? ')
+if q_10 == "a":
+    print("Great. Let me give you my cashapp ")
+elif q_10 == "b":
+    print('Great. Let me give you my cashapp')
+else:
+    print("Get lost then")
+for reply in kiesha_reply:
+    if reply == kiesha_reply[0]:
+        print('Imma tell on you. lol')
+time.sleep(2)
+print("Good bye. Again, my name is Kiesha. I hope you have an amazing day.")
+
+
+
+
+
+
+
+
+    
+
+
